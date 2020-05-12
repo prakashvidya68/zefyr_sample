8653937adc808bcf98a4a0056ceda05d61eba9cf
vidya@headstrt.com <prakashvidya68@gmail.com>
2020-05-12 16:12:33 +0530

    sample

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:zefyr/zefyr.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ZefyrController controller = new ZefyrController(NotusDocument());
+  FocusNode nn = new FocusNode();
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: ZefyrScaffold(
+        child: ZefyrEditor(
+          // imageDelegate: MyAppZefyrImageDelegate(title),
+          autofocus: true,
+          controller: controller,
+          focusNode: nn,
+          padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+        ),
+      ),
+    );
+  }
+}
